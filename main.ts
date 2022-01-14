66968c01deb028a6bff6ed842f6765168c9c4f12
eolavegarc <93262443+eolavegarc@users.noreply.github.com>
2022-01-14 14:22:30 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) > -20) {
+        basic.showLeds(`
+            # . . . #
+            . # # # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    }
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -20) {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+radio.setGroup(100)
+radio.setTransmitPower(3)
 basic.forever(function () {
-	
+    radio.sendNumber(0)
 })
